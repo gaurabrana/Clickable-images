cef8eaa81b3cba28aeb13f00bfa322b8d0757fab
Gaurab Rana <ericwinty90@gmail.com>
2021-03-08 22:15:49 +0545

    Coding Challenge

--- Readme.md
+++ Readme.md
@@ -1,2 +1,2 @@
-## Changed the floating button action
-![](app/src/main/res/drawable/task3.gif)
+## Coding Challenge
+![](app/src/main/res/drawable/coding_challenge.gif)
--- app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -2,13 +2,19 @@ package com.example.droidcafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
-
+    private TextView txtview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        txtview = findViewById(R.id.order_textview);
+        Intent intent = getIntent();
+        String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        txtview.setText(message);
     }
 }
